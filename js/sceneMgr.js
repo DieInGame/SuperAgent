18ec28661a63f7229e23aea179c08f0f79a01f82
zhe13-pc <lovewutianzhe@cuc.edu.cn>
2016-04-23 12:44:00 +0800

    边缘检测有问题

--- js/sceneMgr.js
+++ js/sceneMgr.js
@@ -24,7 +24,7 @@ class SceneManager{
 		for(let x = 0 ; x < col ; x++ ) {
 			maze[x] = new Array();
 			for(let y = 0 ; y < row ; y++) {
-				// 标记
+				// 标记 0 空，1 路径，2 死点，3 起点，4 终点
 				if(x == parseInt(col/2,10) && y == 0){
 					
 					// agent
@@ -49,7 +49,8 @@ class SceneManager{
 							
 			}
 		}
-		console.log("maze",maze);
+		// console.log("maze",maze);
+		this._maze = maze;
 	}
 	
 	
@@ -68,10 +69,107 @@ class SceneManager{
 	*/
 	
 	// 深度优先生成算法
-	DFS(maze){
+	DFS(maze = this._maze){
+		var col = maze.length;
+		var row = maze[0].length;
+		var start = {x:Math.floor(col/2),y:0};
+		
+		var path  = new Array();
+		var impasse  = new Array();
+		var rd = this._renderer;
+		
+		findPath();
+		console.log(path,impasse);
+		// for(let x in path){
+		// 	this._renderer.renderShape("rect","green",{x:path[x].x*this._unit.w + 10,y:10 + path[x].y*this._unit.h},
+		// 			{w:this._unit.w - 20,h: this._unit.h - 20});
+		// }
+		
+		// for(let x in impasse){
+		// 	this._renderer.renderShape("rect","red",{x:impasse[x].x*this._unit.w + 10,y:10 + impasse[x].y*this._unit.h},
+		// 			{w:this._unit.w - 20,h: this._unit.h - 20});
+		// }
+		
+		// 生成路径
+		function findPath(){
+			var next  = getIndex(start.x , start.y );
+			if( maze[next.x][next.y] == 0){
+				path.push(next);
+				maze[next.x][next.y] = 1;
+				draw("green",next.x,next.y);
+				start = next;
+				// 递归
+				findPath();
+			}else if(maze[next.x][next.y] == 1 || maze[next.x][next.y] == 2 || maze[next.x][next.y] == 3){
+				// 当下一个点不为空 且不是终点，检测是否有路可走
+				if(isTrap(start.x,start.y)){
+					impasse.push(start);
+					maze[start.x][start.y] = 2;
+					draw("red",start.x,start.y);
+					start = path.pop();
+				}
+				
+				findPath();
+				
+			}else if(maze[next.x][next.y] == 4){
+				return{
+					path:path,
+					impasse:impasse
+				}
+				
+			}
+		}
+		
 		// 获取方向
 		function getDir() {
-			
+			var dir = ["South","East","North","West"];
+			var x   = Math.floor(Math.random()*(dir.length));
+			return dir[x];
+		}
+		
+		// 获取坐标
+		function getIndex(x,y){
+			var d = getDir();
+			var ny = y;
+			var nx = x;
+			switch (d) {
+				case "South":
+					ny --;
+					break;
+			    case "North":
+					ny ++;
+					break;
+				case "West":
+					nx --;
+					break;
+			    case "East":
+					nx ++;
+					break;
+				
+				default:
+					break;
+			}
+			// if(nx < 0 || ny < 0 || nx >= col || ny >= row){
+			// 	getIndex(x,y);
+			// }
+			if(nx < 0) nx =0;
+			if(ny < 0) ny =0;
+			if(nx >= col) nx = col -1;
+			if(ny >= row) ny = row -1;
+			var new_pos = {x:nx,y:ny};
+			return new_pos;
+		}
+		
+		// 检查是否无路可走
+		function isTrap(x,y){
+			if( maze[x-1][y] == 0 || maze[x][y+1] == 0 || maze[x+1][y] == 0 || maze[x][y-1] == 0 )
+			return false;
+			else return true;
+		}
+		
+		function draw(color,x,y){
+			rd.renderShape("rect",color,{x:x*90 + 10,y:10 + y*90},
+					{w:70,h: 70});
 		}
 	}
 	
